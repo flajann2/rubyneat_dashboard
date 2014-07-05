0aed537d05868b8c7e121d0d72a0f2fded470ab8
Fred Mitchell <lordalveric@yahoo.com>
2014-07-05 11:27:22 +0200

    updated for handling the new changes in hooks -- in progress

--- lib/rubyneat_dashboard/rubyneat_dsl.rb
+++ lib/rubyneat_dashboard/rubyneat_dsl.rb
@@ -2,17 +2,22 @@
 = Dashboard DSL for RubyNEAT
 Additions to the RubyNEAT DSL for hooking into the Dashboard.
 =end
+require 'pp'
 
 module NEAT
   module DSL
     def dashboard(&block)
       Dashboard::run_dashboard!
       block.() if block_given?
-      NEAT::controller.pre_exit_func = Proc.new {
+      NEAT::controller.pre_exit_add do
         puts "Dashboard waiting for user to exit. Or you may do a ^C."
         Dashboard::join!
         puts "Dashboard exited."
-      }
+      end
+
+      NEAT::controller.end_run_add  do |c|
+        puts 'Dashboard end_run called.'
+      end
     end
   end
 end
